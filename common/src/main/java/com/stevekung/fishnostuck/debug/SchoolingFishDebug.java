9bda8f17cde5141e539780ab6b1815cd9089665c
SteveKunG <mccommander_minecraft@hotmail.com>
2024-05-09 16:55:17 +0700

    1.19.2 support

--- common/src/main/java/com/stevekung/fishnostuck/debug/SchoolingFishDebug.java
+++ common/src/main/java/com/stevekung/fishnostuck/debug/SchoolingFishDebug.java
@@ -2,7 +2,7 @@ package com.stevekung.fishnostuck.debug;
 
 import com.stevekung.fishnostuck.mixin.debug.accessor.AbstractSchoolingFishAccessor;
 import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.animal.AbstractSchoolingFish;
 
 public class SchoolingFishDebug
@@ -42,7 +42,7 @@ public class SchoolingFishDebug
             }
 
             fish.setCustomNameVisible(true);
-            fish.setCustomName(new TextComponent(text));
+            fish.setCustomName(Component.literal(text));
         }
     }
 
