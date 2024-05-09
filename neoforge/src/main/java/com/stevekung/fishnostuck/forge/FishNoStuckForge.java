7510be78d0ea81fe2d8fc1dad10b00adde109b24
SteveKunG <mccommander_minecraft@hotmail.com>
2024-05-09 17:15:06 +0700

    1.20.4 support + NeoForge (untested)

---
+++ neoforge/src/main/java/com/stevekung/fishnostuck/forge/FishNoStuckForge.java
@@ -0,0 +1,13 @@
+package com.stevekung.fishnostuck.forge;
+
+import com.stevekung.fishnostuck.FishNoStuck;
+import net.neoforged.fml.common.Mod;
+
+@Mod("fishnostuck")
+public class FishNoStuckForge
+{
+    static
+    {
+        FishNoStuck.init();
+    }
+}
